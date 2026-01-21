0a545407a2532f0277707b8a986193aa1b9d949d
Ormazabal Nascimento <ormazabalnascimento@gmail.com>
2026-01-20 23:23:01 -0300

    feat: validacoes da classe Partida

--- ZebraBet.API/Models/Partida.cs
+++ ZebraBet.API/Models/Partida.cs
@@ -2,10 +2,51 @@
 
 public class Partida
 {
+    public Partida(string equipeVisitante, 
+                    string equipeMandante, 
+                    int golsVisitante, 
+                    int golsMandante, 
+                    DateTime dataPartida)
+    {
+        EquipeVisitante = equipeVisitante;
+        EquipeMandante = equipeMandante;
+        GolsVisitante = golsVisitante;
+        GolsMandante = golsMandante;
+        DataPartida = dataPartida;
+
+        Validar();
+    }
     public string EquipeVisitante { get; private set; }
     public string EquipeMandante { get; private set; }
-    public string GolsVisitante { get; private set; }
-    public string GolsMandante { get; private set; }
-    public string DataPartida { get; private set; }
-    
+    public int GolsVisitante { get; private set; }
+    public int GolsMandante { get; private set; }
+    public DateTime DataPartida { get; private set; }
+
+    private void Validar()
+    {
+        if (string.IsNullOrEmpty(EquipeVisitante))
+        {
+            throw new ArgumentException("Equipe visitante vazia");
+        }
+
+        if (string.IsNullOrEmpty(EquipeMandante))
+        {
+            throw new ArgumentException("Equipe mandante vazia");
+        }
+
+        if (GolsVisitante < 0)
+        {
+            throw new ArgumentException("Gols visitante não pode ser negativo");
+        }
+
+        if (GolsMandante < 0)
+        {
+            throw new ArgumentException("Gols mandante não pode ser negativo");
+        }
+
+        if(DataPartida.Date <= DateTime.Now.Date)
+        {
+            throw new ArgumentException("Data da partida deve ser maior que a data atual");
+        }
+    }
 }
--- ZebraBet.API/obj/Debug/net10.0/ZebraBet.API.AssemblyInfo.cs
+++ ZebraBet.API/obj/Debug/net10.0/ZebraBet.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZebraBet.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5350097b0475fbd0d93d3553d66f11bd7272506e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36924fd66fe3ea62a27636efad4db995e0c4a014")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZebraBet.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZebraBet.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
