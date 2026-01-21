26aabb5b07e0e1093d32a258fc64625d39d79e4d
Ormazabal Nascimento <ormazabalnascimento@gmail.com>
2026-01-20 22:42:50 -0300

    feat: validacoes da classe Estado

--- ZebraBet.API/Models/Estado.cs
+++ ZebraBet.API/Models/Estado.cs
@@ -2,6 +2,40 @@
 
 public class Estado
 {
+
+    public Estado(
+        string nome,
+        string sigla)
+    {
+        Nome = nome;
+        Sigla = sigla;
+
+        Validar();
+    }
+
     public string Nome { get; private set; }
     public string Sigla { get; private set; }
+
+    private void Validar()
+    {
+        if (Nome.Length > 20)
+        {
+            throw new ArgumentException("Nome acima de 20 caracteres");
+        }
+
+        if (Sigla.Length != 2)
+        {
+            throw new ArgumentException("Sigla deve possuir 2 caracteres");
+        }
+
+        if (string.IsNullOrEmpty(Nome))
+        {
+            throw new ArgumentException("Nome vazio");
+        }
+
+        if (string.IsNullOrEmpty(Sigla))
+        {
+            throw new ArgumentException("Sigla vazio");
+        }
+    }
 }
